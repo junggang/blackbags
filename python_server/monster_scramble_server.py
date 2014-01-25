2b3321c52a36348f33b3b2886dcabfb2f8d8e879
wooq <wooq@nhnnext.org>
2014-01-25 07:26:44 -0800

    필요한 함수 목록 추가

--- python_server/monster_scramble_server.py
+++ python_server/monster_scramble_server.py
@@ -1,13 +1,84 @@
 # -*- coding: utf-8 -*-
+import redis
 from flask import Flask,url_for, session, escape, request, redirect ,render_template
 
-app = Flask(__name__)
 
+#################################################
+#				matching thread 관련				#
+#################################################
 # matching thread는 서버가 시작되면 같이 실행된다.
 # 리스트에 있는 사람들이 바뀔 때마다 현재 리스트에 있는 사람들을 가지고 게임 채널 생성하고 채널 테이블을 레디스에 생성
 
+
+
+#################################################
+#				gameLogic 관련					#
+#################################################
+
 # game logic은 게임의 인풋이 도착하면 redis에 있는 데이터를 가져와서 업데이트 시키고 다시 저장한다
 # 모든 인풋은 유효성 검사가 필수
+
+def SCSelectCharacter(gameChannelId, playerId, characterId):
+	# 인자로 받은 game channel의 playerId에 해당하는 유저의 선택 character를 업데이트
+	return #gameData
+
+
+def SCSelctMap(gameChannelId, playerId, mapId):
+	# 인자로 받은 game channel의 map 정보를 업데이트
+	return #gameData
+
+
+def SCReady(gameChannelId, playerId):
+	# 인자로 받은 game channel의 playerId에 해당하는 유저의 대기 상태 업데이트
+	return #gameData
+
+
+def PCStartTurn(gameChannelId):
+	# 인자로 받은 game channel의 새로운 턴 시작
+	return #gameData
+
+
+def PCDrawLine(gameChannelId, playerId, posI, posJ):
+	# 인자로 받은 game channel의 새로운 턴 시작
+	return #gameData
+
+
+def PCUpdateGameResult(gameChannelId):
+	# 인자로 받은 game channel의 데이터를 기반으로 결과 계산
+	return #gameData
+
+
+# game data를 string으로 바꾸고 반대로 파싱하는 함수 필요
+# client에 전송할 때 http를 사용하니까 string이어야 하는데 
+# 응답 속도를 빠르게 하려면 redis에 저장할 때도 string 형태로 저장해 두는 게 좋을지도
+# gameData는 struct 구조로 작성하거나 dic 구조로 작성
+def makeString(gameData):
+	resultString
+
+	return resultString
+
+
+def makeGameData(srcString):
+	gameData
+
+	return gameData
+
+
+
+#################################################
+#					redis 관련 					#
+#################################################
+global gRedis
+
+def connect_redis():
+	return redis.StrictRedis(host=REDIS_ADDRESS, port=REDIS_PORT, db=0)
+
+
+
+#################################################
+#					flask 관련 					#
+#################################################
+app = Flask(__name__)
 
 @app.route('/login', methods=['POST','GET'])
 def login():
@@ -37,7 +108,7 @@ def joinUpdate():
 		return '서버에서 알 수 없는 요청데이터가 있습니다.(잘못된 요청일 수도 있고요)\n'
 
 
-@app.route('/selectCharacter/', methods=['POST','GET'])
+@app.route('/select_character/', methods=['POST','GET'])
 def selectCharacter():
 	# 보낸 유저의 session 정보를 바탕으로 어떤 game channel에 반영할 지 판단(redis 안에 저장된 user 정보 참조)
 	# 해당 game channel의 데이터를 불러와서 게임 데이터에서 플레이어의 캐릭터id 정보를 업데이트한다.
@@ -51,8 +122,8 @@ def selectCharacter():
 		return 'error code'
 
 
-@app.route('/map_select/', methods=['POST','GET'])
-def mapSelect():
+@app.route('/select_map/', methods=['POST','GET'])
+def selectMap():
 	# 보낸 유저의 session 정보를 바탕으로 어떤 game channel에 반영할 지 판단(redis 안에 저장된 user 정보 참조)
 	# 해당 game channel의 데이터를 불러와서 현재 선택된 맵 정보를 업데이트한다.
 	# 업데이트 결과를 redis에 저장하고, 요청을 보낸 유저에게는 변경된 게임 데이터를 바로 전송한다.
@@ -140,7 +211,10 @@ def gameEnd():
 		return 'error code'
 
 
+
 if __name__ == '__main__':
+	gRedis = connect_redis()
+
     app.debug = True
     app.secret_key = '\xab\x11\xcb\xdb\xf2\xb9\x0e\xd9N\xbd\x17$\x07\xc9H\x19\x96h\x8a\xf2<`-A'
     app.run()
