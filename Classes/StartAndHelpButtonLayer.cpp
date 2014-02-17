fb11905bf27a266a320f49317ae9b4f473123b5c
blastingzone <blastingzone@naver.com>
2014-02-17 05:29:18 -0800

    Multiplay Scene Change 로직 구성 중

--- Classes/StartAndHelpButtonLayer.cpp
+++ Classes/StartAndHelpButtonLayer.cpp
@@ -6,7 +6,9 @@
 
 USING_NS_CC;
 
-const int HelpPopupLayerId = 1000;
+const int HELP_POPUPLAYER_TAG = 1000;
+const int START_BUTTON_TAG = 2000;
+const int READY_BUTTON_TAG = 3000;
 
 bool CStartAndHelpButtonLayer::init()
 {
@@ -19,15 +21,15 @@ bool CStartAndHelpButtonLayer::init()
 	CCSize visibleSize = CCDirector::sharedDirector()->getVisibleSize();
 
 	//// start Button
-	//// 조심해!! 지금 내가 방장인지 아닌지 여부를 같이 확인해야 해!
 	if ( CGameManager::GetInstance()->IsOnlineMode() && !CGameManager::GetInstance()->IsChannelMaster() )
 	{
 		m_StartButton = CCMenuItemImage::create(
 			"image/SETTING_ready_unselected.png",
 			"image/SETTING_ready_selected.png",
 			this,
-			menu_selector(CStartAndHelpButtonLayer::StartButtonCallBack)
+			menu_selector(CStartAndHelpButtonLayer::ReadyButtonCallBack)
 			);
+		m_StartButton->setTag(READY_BUTTON_TAG);
 	}
 	else
 	{
@@ -37,6 +39,7 @@ bool CStartAndHelpButtonLayer::init()
 			this,
 			menu_selector(CStartAndHelpButtonLayer::StartButtonCallBack)
 			);
+		m_StartButton->setTag(START_BUTTON_TAG);
 	}
 	
 	// at init, Start button is disabled
@@ -90,7 +93,8 @@ void CStartAndHelpButtonLayer::ReadyButtonCallBack( CCObject* pSender )
 	CCMessageBox("You pressed the close button. Windows Store Apps do not implement a close button.","Alert");
 #else
 	// 서버로 레디 신호를 보낸다.
-
+	CGameManager::GetInstance()->StartGame();
+	// Scene Change는 GetCurrentScene 을 통해서 비교 / Online 에서는 임의로 Scene을 바꾸지 않는다. 
 #if (CC_TARGET_PLATFORM == CC_PLATFORM_IOS)
 	//exit(0);
 #endif
@@ -107,7 +111,7 @@ void CStartAndHelpButtonLayer::HelpButtonCallBack( CCObject* pSender )
 	
 	if (newLayer != NULL)
 	{
-		newLayer->setTag(HelpPopupLayerId);
+		newLayer->setTag(HELP_POPUPLAYER_TAG);
 		this->addChild(newLayer);
 	}
 
@@ -119,9 +123,10 @@ void CStartAndHelpButtonLayer::HelpButtonCallBack( CCObject* pSender )
 
 void CStartAndHelpButtonLayer::update()
 {
-	// Online :: StartButton Condition
+	// Online Condition
 	if ( CGameManager::GetInstance()->IsOnlineMode() )
 	{
+		// 1. Start Button?
 		// 내가 방장이고 모두가 Ready 했을 때 StartButton을 활성화 시킨다.
 		if ( CGameManager::GetInstance()->IsChannelMaster() && 
 			CGameManager::GetInstance()->IsAllReady() )
@@ -132,7 +137,21 @@ void CStartAndHelpButtonLayer::update()
 		{
 			m_StartButton->setEnabled(false);
 		}
+		// 2. Ready Button?
+		// 내가 방장이 아니고 캐릭터를 골랐을 때 ReadyButton을 활성화 시킨다.
+		if ( !CGameManager::GetInstance()->IsChannelMaster() 
+			&& CGameManager::GetInstance()->GetCharacterIdByPlayerId( CGameManager::GetInstance()->GetMyPlayerId() ) != -1 )
+		{
+			m_StartButton->setEnabled(true);
+		}
+		else
+		{
+			m_StartButton->setEnabled(false);
+		}
+		// 3. Scene Change && Game Start??
+		// 서버에서 응답이 돌아오면 Scene Change
 	}
+
 	// Single :: StartButton Condition
 	else
 	{
